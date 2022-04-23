79980fdff9a89bcaa85d87f00705165d8b16d027
vfgca.com <vfgca@naver.com>
2022-04-23 14:16:48 +0900

    test: https://example.com

--- hello.ts
+++ hello.ts
@@ -2,3 +2,15 @@ console.log("Hello, world!");
 
 console.log("%cHello, world!", "background-color: yellow; color: black; font-weight: bold;");
 
+
+
+
+let resp = await fetch("https://example.com");
+
+console.log(resp.status); // 200
+console.log(resp.headers.get("Content-Type")); // "text/html"
+console.log(await resp.text()); // "Hello, World!"
+
+
+
+
