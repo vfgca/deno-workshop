64a49b16eee9aa9d7a166a4abc69e9d19252a247
vfgca.com <vfgca@naver.com>
2022-04-27 00:38:23 +0900

    test: deno fmt

--- hello.ts
+++ hello.ts
@@ -1,25 +1,21 @@
 console.log("Hello, world!");
 
-console.log("%cHello, world!", "background-color: yellow; color: black; font-weight: bold;");
-
-
-
+console.log(
+  "%cHello, world!",
+  "background-color: yellow; color: black; font-weight: bold;",
+);
 
 let res = await fetch("https://example.com");
 
 console.log("========");
-console.log('* res.status:', res.status);
-console.log('* res.headers.get("Content-Type"):', res.headers.get("Content-Type"));
+console.log("* res.status:", res.status);
+console.log(
+  '* res.headers.get("Content-Type"):',
+  res.headers.get("Content-Type"),
+);
 console.log("--------");
-console.log('* res.text():');
+console.log("* res.text():");
 console.log("%c[", "color: green;");
 console.log(await res.text());
 console.log("%c]", "color: green;");
 console.log("========");
-
-
-
-
-
-
-
