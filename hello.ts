02657a3847b2e4ec9d17c1374dc6e7c9c53ce02a
vfgca.com <vfgca@naver.com>
2022-05-03 14:33:48 +0900

    test: permission

--- hello.ts
+++ hello.ts
@@ -1,7 +1,7 @@
 const titleStyle = "font-weight: bold;";
 const lineStyle = "color: green;";
 
-console.log("%c//////// console", titleStyle);
+console.log("\n%c//////// console", titleStyle);
 
 console.log("Hello, world!");
 
@@ -10,7 +10,7 @@ console.log(
   "background-color: yellow; color: black; font-weight: bold;",
 );
 
-console.log("%c//////// fetch", titleStyle);
+console.log("\n%c//////// fetch", titleStyle);
 
 let res = await fetch("https://example.com");
 
@@ -26,9 +26,35 @@ console.log("%c--------", lineStyle);
 console.log(await res.text());
 console.log("%c--------", lineStyle);
 
-console.log("%c//////// globalThis", titleStyle);
+console.log("\n%c//////// globalThis", titleStyle);
 
 console.log("* globalThis:");
 console.log("%c--------", lineStyle);
 console.log(globalThis);
 console.log("%c--------", lineStyle);
+
+console.log("\n%c//////// permission", titleStyle);
+
+const permission_desc_write = { name: "write" } as const;
+console.log(
+  '* { name: "write" }:',
+  await Deno.permissions.query(permission_desc_write),
+);
+
+const permission_desc_net1 = { name: "net" } as const;
+console.log(
+  '* { name: "net" }:',
+  await Deno.permissions.query(permission_desc_net1),
+);
+
+const permission_desc_net2 = { name: "net", host: "127.0.0.1:8000" } as const;
+console.log(
+  '* { name: "net", host: "127.0.0.1:8000" }:',
+  await Deno.permissions.query(permission_desc_net2),
+);
+
+const permission_desc_hrtime = { name: "hrtime" } as const;
+console.log(
+  '* { name: "hrtime" }:',
+  await Deno.permissions.query(permission_desc_hrtime),
+);
