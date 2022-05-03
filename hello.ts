5606d63d224e3572444e6fb79e206f2d58fe7204
vfgca.com <vfgca@naver.com>
2022-05-03 09:22:11 +0900

    output structure

--- hello.ts
+++ hello.ts
@@ -1,3 +1,7 @@
+const titleStyle = "font-weight: bold;";
+
+console.log("%c//////// console", titleStyle);
+
 console.log("Hello, world!");
 
 console.log(
@@ -5,17 +9,25 @@ console.log(
   "background-color: yellow; color: black; font-weight: bold;",
 );
 
+console.log("%c//////// fetch", titleStyle);
+
 let res = await fetch("https://example.com");
 
-console.log("========");
 console.log("* res.status:", res.status);
+
 console.log(
   '* res.headers.get("Content-Type"):',
   res.headers.get("Content-Type"),
 );
-console.log("--------");
+
 console.log("* res.text():");
-console.log("%c[", "color: green;");
+console.log("%c--------", "color: green;");
 console.log(await res.text());
-console.log("%c]", "color: green;");
-console.log("========");
+console.log("%c--------", "color: green;");
+
+console.log("%c//////// globalThis", titleStyle);
+
+console.log("* globalThis:");
+console.log("%c--------", "color: green;");
+console.log(globalThis);
+console.log("%c--------", "color: green;");
